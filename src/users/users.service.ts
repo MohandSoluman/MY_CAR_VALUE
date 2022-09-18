c1814b48d8ea0e1c9e6924220d2bdb8ae203d9a0
MohandSoluman <engmohandabosoluman@gmail.com>
2022-09-18 22:39:42 +0200

    add auth files

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { throwError } from 'rxjs';
 import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 
 @Injectable()
@@ -10,16 +9,18 @@ export class UsersService {
 
   create(email: string, password: string) {
     const user = this.repo.create({ email, password });
+
     return this.repo.save(user);
   }
 
   findOne(id: number) {
-    return this.repo.findOneBy({ id });
+    return this.repo.findOne(id);
   }
 
   find(email: string) {
-    return this.repo.findBy({ email });
+    return this.repo.find({ email });
   }
+
   async update(id: number, attrs: Partial<User>) {
     const user = await this.findOne(id);
     if (!user) {
@@ -28,6 +29,7 @@ export class UsersService {
     Object.assign(user, attrs);
     return this.repo.save(user);
   }
+
   async remove(id: number) {
     const user = await this.findOne(id);
     if (!user) {
